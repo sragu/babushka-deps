3a63b22ac9a98af49479c3bcd87ada6ccc6dd4c3
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2015-07-24 17:34:58 +0100

    set function keys to be default on the keyboard

--- prefs.rb
+++ prefs.rb
@@ -4,6 +4,13 @@ dep 'set caps lock to control', template: 'task' do
   end
 end
 
+dep 'set function keys to be default', template: 'task' do
+  on :osx do
+    run { shell 'osascript', File.expand_path('../lib/set_function_keys_by_default.applescript', __FILE__) }
+  end
+end
+
 dep 'prefs' do
   requires 'set caps lock to control'
+  requires 'set function keys to be default'
 end
