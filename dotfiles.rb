e05512645f4a103cf42aff9b1925434a8a466015
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2015-07-25 11:46:01 +0100

    add sublime commandline

--- dotfiles.rb
+++ dotfiles.rb
@@ -1,11 +1,11 @@
 dep 'zsh config', template: 'task' do
   met? { "~/.zshrc".p.exists? }
-  run { log_shell "set zshrc", "cp ./dotfiles/zshrc.erb ~/.zshrc" }
+  run { log_shell "set zshrc", "cp ./dotfiles/zshrc.erb ~/.zshrc", :cd => '~/.babushka/deps' }
 end
 
 dep 'git global config', template: 'task' do
   met? { "~/.gitconfig".p.exists? }
-  run { log_shell "set git config", "cp ./dotfiles/gitconfig.erb ~/.gitconfig" }
+  run { log_shell "set git config", "cp ./dotfiles/gitconfig.erb ~/.gitconfig", :cd => '~/.babushka/deps' }
 end
 
 #create a template for dotfiles or copy all files in the directory
